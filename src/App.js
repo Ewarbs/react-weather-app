a2b6502e41f9dd3cf1ad59af03407ea8cd81d1ad
Ewarbs <69345974+Ewarbs@users.noreply.github.com>
2020-11-07 13:00:15 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 export default function App() {
 return (
     <div className="App">
-     Weather App
+      <div className="container">
+    <h1>Weather App</h1> 
+<footer>
+  This Project has been created by Emily Warburton and is{" "}
+     <a href="https://github.com/Ewarbs/react-weather-app" target="blank">
+  open-sourced
+     </a>
+     </footer>
+     </div>
     </div>
   );
 }
