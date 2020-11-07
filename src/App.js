d4323a25856643594051edcb9c657f5b0ced2ce9
Ewarbs <69345974+Ewarbs@users.noreply.github.com>
2020-11-07 13:14:42 +0000

    Added a new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
 return (
     <div className="App">
       <div className="container">
     <h1>Weather App</h1> 
+    <Weather />
 <footer>
   This Project has been created by Emily Warburton and is{" "}
      <a href="https://github.com/Ewarbs/react-weather-app" target="blank">
