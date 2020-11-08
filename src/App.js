1ccfb40265f44436df0b9c67f88cb53ad86df02e
Ewarbs <69345974+Ewarbs@users.noreply.github.com>
2020-11-08 13:55:14 +0000

    Made changes to app CSS styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
 return (
     <div className="App">
       <div className="container">
-    <h1>Weather App</h1> 
+    <h1></h1> 
     <Weather />
 <footer>
   This Project has been created by Emily Warburton and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,10 @@ export default function Weather(){
           <form>
               <div className="row">
                   <div className="col-9">
-             <input type="search" placeholder="Enter a city.." className="form-control" />
+             <input type="search" placeholder="Enter a city.." className="form-control" autoFocus="on" />
              </div>
              <div className="col-3">
-             <input type="submit" value="Search" className="btn btn-primary" />
+             <input type="submit" value="Search" className="btn btn-primary w-100" />
              </div>
              </div>
           </form>
@@ -20,8 +20,13 @@ export default function Weather(){
         </ul>
         <div className="row">
             <div className="col-6">
-              <img src="https://duckduckgo.com/assets/weather/svg/new/cloudy.svg" alt="Mostly cloudy" />
-              6°C
+                <div className="clearfix">
+              <img src="https://duckduckgo.com/assets/weather/svg/new/cloudy.svg" alt="Mostly cloudy" className="float-left" />
+            <div className="float-left">
+            <span className="temperature">6</span>
+            <span className="units">°C</span>
+            </div>
+            </div>
             </div>
             <div className="col-6">
                 <ul>
